42720114b029e56dde2324a70bbb9a926f17a562
Ranga Duriseti <RDuriseti@aceinfosolutions.com>
2019-11-21 17:45:35 -0500

    CORS Issue Resolution

--- Source/src/main/java/com/aceinfo/commonservices/security/api/cognito/config/SecurityConfig.java
+++ Source/src/main/java/com/aceinfo/commonservices/security/api/cognito/config/SecurityConfig.java
@@ -33,21 +33,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
 		http.cors().and().csrf().disable() // We don't need CSRF for JWT based authentication
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().antMatchers("/actuator/**").permitAll()
 				.antMatchers("/healthcheck").permitAll()
-				//.antMatchers("/login/**").permitAll()
-				.antMatchers(HttpMethod.GET, "/login/**").permitAll().antMatchers("/authenticate").permitAll().antMatchers("/validatesessiontoken").permitAll()
-				.antMatchers("/confirmnewuser").permitAll().antMatchers("/forgotPassword/**").permitAll().antMatchers("/confirmPassword/**").permitAll().antMatchers("/test/**")
-				.permitAll().antMatchers("/swagger-resources/configuration/ui").permitAll().antMatchers("/swagger*").permitAll().antMatchers(HttpMethod.GET,
-						// "/",
-						"/v2/api-docs", // swagger
-						"/webjars/**", // swagger-ui webjars
-						"/swagger-resources/**", // swagger-ui resources
-						"/configuration/**", // swagger configuration
-						"/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js")
-				.permitAll().anyRequest().authenticated() // Protected API End-points
+				.antMatchers(HttpMethod.GET, "/login/**").permitAll()
+				.antMatchers("/authenticate").permitAll()
+				.antMatchers("/validatesessiontoken").permitAll()
+				.antMatchers("/confirmnewuser").permitAll()
+				.antMatchers("/forgotPassword/**").permitAll()
+				.antMatchers("/confirmPassword/**").permitAll()
+				.antMatchers("/test/**").permitAll()
+				.antMatchers("/swagger-resources/configuration/ui").permitAll()
+				.antMatchers("/swagger*").permitAll()
+				.antMatchers(HttpMethod.GET,"/v2/api-docs", "/webjars/**", "/swagger-resources/**", "/configuration/**", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js").permitAll()
+				.anyRequest().authenticated() // Protected API End-points
 				.and().addFilterBefore(authTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 				//.oauth2Login();
 	}
