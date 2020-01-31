6bc4dd22cf79dc8f647db0ec3daa4ea14fb172f5
Ranga Duriseti <RDuriseti@aceinfosolutions.com>
2020-01-31 12:59:35 -0500

    Update for New Openshift Environment

--- Source/src/main/java/com/aceinfo/commonservices/security/api/cognito/config/CognitoConfig.java
+++ Source/src/main/java/com/aceinfo/commonservices/security/api/cognito/config/CognitoConfig.java
@@ -1,19 +1,13 @@
 package com.aceinfo.commonservices.security.api.cognito.config;
 
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSSessionCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 
@@ -25,7 +19,7 @@ public class CognitoConfig {
 	private String								awsAccessKey;
 	@Value("${AWS_SECRET_ACCESS_KEY}")
 	private String								awsSecretKey;
-	@Value("${aws_region}")
+	@Value("${AWS_REGION}")
 	private String								awsRegion;
 
 	
